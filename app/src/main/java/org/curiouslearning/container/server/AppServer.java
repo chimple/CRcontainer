49710ccfdfa3f5d72f8f952f627b62963fbe8e8b
prayagraj-sutara <prayagraj@sutara.org>
2025-06-18 16:19:30 +0530

    fetching assets from internal storage

--- app/src/main/java/org/curiouslearning/container/server/AppServer.java
+++ app/src/main/java/org/curiouslearning/container/server/AppServer.java
@@ -2,6 +2,7 @@ package org.curiouslearning.container.server;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,22 +12,26 @@ import fi.iki.elonen.NanoHTTPD;
 public class AppServer extends NanoHTTPD {
     private final Context context;
     private final String baseAssetFolder;
+    private final String TAG = "AppServer";
 
     public AppServer(Context context, int port, String baseAssetFolder) {
         super(port);
         this.context = context;
         this.baseAssetFolder = baseAssetFolder;
+
         try {
             context.getAssets().open(baseAssetFolder + "/bundle.js").close();
             android.util.Log.d("LocalWebServer", "bundle.js is accessible in " + baseAssetFolder + "!");
         } catch (Exception e) {
             android.util.Log.e("LocalWebServer", "bundle.js is NOT accessible in " + baseAssetFolder + "!", e);
         }
+//        Log.d(TAG, "rootDirectory is "+ )
     }
 
     @Override
     public Response serve(IHTTPSession session) {
         String uri = session.getUri();
+        android.util.Log.d("LocalWebServer", "Requested asset-----------" + uri);
         if (uri.equals("/")) {
             uri = "/index.html";
         }
@@ -39,16 +44,32 @@ public class AppServer extends NanoHTTPD {
             String mime = resolveMimeType(uri);
             return newChunkedResponse(Response.Status.OK, mime, is);
         } catch (IOException e) {
-            // If not found in baseAssetFolder, try lessonAsset
+            // Try lessonAsset in assets
             String lessonAssetPath = "lessonAsset" + uri;
-            android.util.Log.d("LocalWebServer", "Asset not found in baseAssetFolder, trying lessonAsset: " + lessonAssetPath);
+            android.util.Log.d("LocalWebServer",
+                    "Asset not found in baseAssetFolder, trying lessonAsset: " + lessonAssetPath);
             try {
                 AssetManager assetManager = context.getAssets();
                 InputStream is = assetManager.open(lessonAssetPath);
                 String mime = resolveMimeType(uri);
                 return newChunkedResponse(Response.Status.OK, mime, is);
             } catch (IOException e2) {
-                android.util.Log.e("LocalWebServer", "Asset not found in lessonAsset either: " + lessonAssetPath, e2);
+                // Try file system (external files dir)
+//                java.io.File storageFile = new java.io.File(context.getExternalFilesDir(null),"assessment" + uri);
+                java.io.File storageFile = new java.io.File(context.getExternalFilesDir(null), uri);//adjust as needed
+                android.util.Log.d("LocalWebServer",
+                        "Asset not found in lessonAsset, trying storage: " + storageFile.getAbsolutePath());
+                if (storageFile.exists() && storageFile.isFile()) {
+                    try {
+                        java.io.FileInputStream fis = new java.io.FileInputStream(storageFile);
+                        String mime = resolveMimeType(uri);
+                        return newChunkedResponse(Response.Status.OK, mime, fis);
+                    } catch (IOException e3) {
+                        android.util.Log.e("LocalWebServer",
+                                "Error reading file from storage: " + storageFile.getAbsolutePath(), e3);
+                    }
+                }
+                android.util.Log.e("LocalWebServer", "Asset not found anywhere: " + lessonAssetPath, e2);
                 return newFixedLengthResponse(Response.Status.NOT_FOUND, "text/plain", "404 Not Found");
             }
         } catch (Exception e) {
